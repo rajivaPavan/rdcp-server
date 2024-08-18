ddd14f8063b3df7eeff6d5447d68c961ac0af16f
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-08-18 17:07:42 +0530

    feat: Add CryptService for password hashing and comparison
    This commit adds the CryptService class to handle password hashing and comparison using bcrypt. The service includes methods for hashing a password and comparing a password with a hash. The salt used for hashing is retrieved from the configuration file. This service will be used for secure password storage and authentication in the application.

---
+++ src/modules/crypt/crypt.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class CryptService {
+
+    private readonly salt : string;
+
+    constructor(private readonly config: ConfigService) {
+        this.salt = this.config.get<string>('SALT');
+    }
+
+    async hashPassword(password: string): Promise<string> {
+        return bcrypt.hash(password, this.salt);
+    }
+
+    async comparePassword(password: string, hash: string): Promise<boolean> {
+        return bcrypt.compare(password, hash);
+    }
+}
