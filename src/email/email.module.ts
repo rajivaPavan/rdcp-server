8c1fd9b6e6fa963291bc01e4483b6a32ffe3cf0a
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-22 22:22:44 +0530

    Add email templates and configure mailer module

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common';
 import { EmailServiceFactory } from './email-service.factory';
 import { IEmailService } from './email.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({  
+      transport: {  
+        host: '<host>',  
+        port: Number('<port>'),  
+        secure: false,  
+        auth: {  
+          user: '<username>',  
+          pass: '<password>',  
+        },  
+      },  
+      defaults: {  
+        from: '"From Name" <from@example.com>',  
+      },  
+      template: {  
+        dir: join(__dirname, 'templates'),  
+        adapter: new EjsAdapter(),  
+        options: {  
+          strict: true,  
+        },  
+      },  
+    }),  
+  ],
   providers: [
     EmailServiceFactory,
     {
