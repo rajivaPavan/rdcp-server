da13ac45efa27f7fafe4b43fa024c002e4b7226c
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-24 00:19:34 +0530

    Added S3ObjectStorageService and add file upload functionality

--- src/responses/responses.controller.ts
+++ src/responses/responses.controller.ts
@@ -1,17 +1,23 @@
 import { Body, Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { ResponsesService } from './responses.service';
+import { S3ObjectStorageService } from './files.service';
 
 @Controller('responses')
 export class ResponsesController {
 
     constructor(
         private readonly responsesService: ResponsesService,
+        private readonly s3ObjectStorageService: S3ObjectStorageService
     ) {}
 
     @Post('submit')
     @UseInterceptors(AnyFilesInterceptor())
     async uploadFile(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body: any) {
         await this.responsesService.submit(body, files);
+        files.forEach(async file => {
+            let name = await this.s3ObjectStorageService.uploadFile(file);
+            console.log(name);
+        });
     }
 }
--- src/responses/responses.module.ts
+++ src/responses/responses.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ResponsesService } from './responses.service';
 import { ResponsesController } from './responses.controller';
+import { S3ObjectStorageService } from './files.service';
 
 @Module({
-  providers: [ResponsesService],
+  providers: [ResponsesService, S3ObjectStorageService],
   controllers: [ResponsesController]
 })
 export class ResponsesModule {}
