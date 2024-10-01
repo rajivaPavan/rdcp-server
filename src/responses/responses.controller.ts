9dc52eb47f2d4d7bb2c99a72614bc2dd0153a5d8
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-10-01 17:59:13 +0530

    refactor: update ResponsesController to handle file uploads for a specific form

--- src/responses/responses.controller.ts
+++ src/responses/responses.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, Req, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { ResponsesService } from './responses.service';
 import { S3ObjectStorageService } from './files.service';
+import { FormId } from 'src/forms/decorators/form-id.decorator';
 
 @Controller('responses')
 export class ResponsesController {
@@ -9,11 +10,17 @@ export class ResponsesController {
     constructor(
         private readonly responsesService: ResponsesService,
         private readonly s3ObjectStorageService: S3ObjectStorageService
-    ) {}
+    ) { }
 
-    @Post('submit')
-    @UseInterceptors(AnyFilesInterceptor())
-    async uploadFile(@UploadedFiles() files: Array<Express.Multer.File>, @Body() body: any) {
+    @Post(':formId/submit')
+    @UseInterceptors(AnyFilesInterceptor({
+
+    }))
+    async uploadFile(
+        @FormId() formId: string,
+        @UploadedFiles() files: Array<Express.Multer.File>,
+        @Body() body: any) {
+
         await this.responsesService.submit(body, files);
         files.forEach(async file => {
             let name = await this.s3ObjectStorageService.uploadFile(file);
