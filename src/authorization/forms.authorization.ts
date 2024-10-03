c0ae43a0f20b9ba690f0a5f5bfa012aef12a4731
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-10-03 20:38:37 +0530

    Refactor form authorization logic and add public form check, update submission endpoints

--- src/authorization/forms.authorization.ts
+++ src/authorization/forms.authorization.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Form } from "src/forms/entities/form.schema";
 import { FormsRepository } from "src/forms/forms.repository";
 
 @Injectable()
@@ -14,5 +15,23 @@ export class FormAuthorization{
 
     privateSubmissionAuth(form: any, userId: string) {
         return true;
+    }
+
+    publicSubmissionAuth(form: Partial<Form>): {
+        authorized: boolean;
+        message?: string;
+    } {
+
+        if (!form.isPublished) {
+            throw new NotFoundException('Form is not found');
+        }
+
+        // if form is not private then anyone can submit a response
+        if (!form.isPrivate)
+            return { authorized: true };
+
+        return {
+            authorized: false,
+        }
     }
 }
