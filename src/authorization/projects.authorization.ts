a3ddf24b4e5ce5d2a9d5ee7e8146f53ce6bf2c26
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-10-14 16:07:12 +0530

    Refactor projects.authorization.ts to add authorization logic for project actions

--- src/authorization/projects.authorization.ts
+++ src/authorization/projects.authorization.ts
@@ -1,13 +1,13 @@
 import { ProjectRoleEnum } from "../projects/entities/project-role.enum";
 import { CollaboratorsRepository } from "../projects/collaborators.repository";
 import { Types } from "mongoose";
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 
 export const projectActions = {
     create_form: [ProjectRoleEnum.MANAGER],
     update_form: [ProjectRoleEnum.MANAGER],
     delete_form: [ProjectRoleEnum.MANAGER],
-    view_forms:[ProjectRoleEnum.MANAGER, ProjectRoleEnum.EDITOR, ProjectRoleEnum.ANALYST, ProjectRoleEnum.ANALYST_VIEW_ONLY],
+    view_forms: [ProjectRoleEnum.MANAGER, ProjectRoleEnum.EDITOR, ProjectRoleEnum.ANALYST, ProjectRoleEnum.ANALYST_VIEW_ONLY],
     view_form: [ProjectRoleEnum.MANAGER],
     edit_form_properties: [ProjectRoleEnum.MANAGER],
     edit_form_schema: [ProjectRoleEnum.MANAGER, ProjectRoleEnum.EDITOR],
@@ -26,17 +26,9 @@ export class ProjectAuthorization {
     // This method will check if the user is a collaborator of the project and if the user has
     // the required role to perform the action.
     async isAuthorized(userId: string, projectId: string, action: ProjectAction): Promise<boolean> {
-        const collaborator = await this.collaboratorsRepository.findOne({
-            project: new Types.ObjectId(projectId),
-            user: new Types.ObjectId(userId),
-        });
 
-        if (!collaborator) {
-            return false;
-        }
-
-        const projectRoles = collaborator.roles;
-
+        const projectRoles = await this.getProjectRoles(userId, projectId);
+        
         // check if the user is an owner of the project, if so, they can perform any action
         if (projectRoles.includes(ProjectRoleEnum.OWNER)) {
             return true;
@@ -45,6 +37,20 @@ export class ProjectAuthorization {
         // required roles to perform the action
         const requiredRoles = projectActions[action];
         return projectRoles.some((role: ProjectRoleEnum) => requiredRoles.includes(role));
+    }
+
+    async getProjectRoles(userId: string, projectId: string): Promise<ProjectRoleEnum[]> {
+        const collaborator = await this.collaboratorsRepository.findOne({
+            project: new Types.ObjectId(projectId),
+            user: new Types.ObjectId(userId),
+        });
+
+        if (!collaborator) {
+            throw new ForbiddenException('User is not a collaborator of the project');
+        }
+
+        const projectRoles = collaborator.roles;
+        return projectRoles;
     }
 
 }
