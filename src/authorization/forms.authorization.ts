78e16a5eee3beaf13791604968e9fe4fa53dce35
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-10-16 13:41:30 +0530

    Refactor forms.authorization.ts to handle private form submission authorization

--- src/authorization/forms.authorization.ts
+++ src/authorization/forms.authorization.ts
@@ -1,29 +1,84 @@
-import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { Form } from "src/forms/entities/form.schema";
 import { FormsRepository } from "src/forms/forms.repository";
+import { ProjectAuthorization } from "./projects.authorization";
+import { NoFormAccessException } from "src/responses/responses.controller";
 
 @Injectable()
-export class FormAuthorization{
+export class FormAuthorization {
 
     constructor(
-        private readonly formsRepository: FormsRepository
-    ) {}
+        private readonly formsRepository: FormsRepository,
+        private readonly projectAuthorization: ProjectAuthorization,
+    ) { }
 
-    async getForm(formId: string) {
+    /**
+     * Retrieves a form by its ID.
+     *
+     * @param formId - The unique identifier of the form to retrieve.
+     * @returns A promise that resolves to the form object if found, or null if not found.
+     */
+    async getForm(formId: string): Promise<Form> {
         return await this.formsRepository.findById(formId);
     }
 
-    privateSubmissionAuth(form: any, userId: string) {
+    /**
+     * Authorizes a user's submission to a form.
+     * 
+     * @param form - The form object to be submitted.
+     * @param userId - The ID of the user attempting to submit the form.
+     * @returns A promise that resolves to an object indicating whether the user is authorized to submit the form and an optional message.
+     * 
+     * @throws UnPublishedFormException - If the form is not published.
+     * @throws NoFormAccessException - If the form is private and the user is neither a collaborator nor a participant.
+     */
+    async privateSubmissionAuth(form: any, userId: string): Promise<{ authorized: boolean; message?: string; }> {
+
+        if (!form.isPublished) {
+            throw new UnPublishedFormException();
+        }
+
+        // if form is not private then anyone can submit a response
+        if (!form.isPrivate)
+            return { authorized: true };
+
+        // check if user is a collaborator of the project
+        const roles = await this.projectAuthorization.getProjectRoles(userId, form.projectId);
+        const isCollaborator = roles.length > 0;
+
+        if (!isCollaborator)
+            throw new NoFormAccessException();
+
+        // check if user is a participant
+
+        if (!await this.isParticipant(userId))
+            throw new NoFormAccessException();
+
+        return {
+            authorized: true,
+        };
+    }
+
+
+    async isParticipant(userId: string) {
+        // TODO: Implement
         return true;
     }
 
+    /**
+     * Determines if a form is available for public submission.
+     *
+     * @param form - A partial form object containing the properties to check for authorization.
+     * @returns An object indicating whether the form is authorized for submission and an optional message.
+     * @throws UnPublishedFormException - If the form is not published.
+     */
     publicSubmissionAuth(form: Partial<Form>): {
         authorized: boolean;
         message?: string;
     } {
 
         if (!form.isPublished) {
-            throw new ConflictException('This form is not accepting responses');
+            throw new UnPublishedFormException();
         }
 
         // if form is not private then anyone can submit a response
@@ -33,5 +88,11 @@ export class FormAuthorization{
         return {
             authorized: false,
         }
+    }
+}
+
+export class UnPublishedFormException extends ConflictException {
+    constructor() {
+        super('This form is not accepting responses');
     }
 }
