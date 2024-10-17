a95b2dc281ea7788e2ae6981d97a2f4e03dc8ab0
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-10-18 02:55:57 +0530

    Refactor auth.e2e-spec.ts to use testAdmin from SeedService

--- test/e2e/auth.e2e-spec.ts
+++ test/e2e/auth.e2e-spec.ts
@@ -3,93 +3,95 @@ import * as request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { initializeE2ETest } from './initializeE2ETest';
 import { access } from 'fs';
+import { SeedService } from 'src/users/seed';
 
 describe('AuthenticationController (e2e)', () => {
-  let app: INestApplication;
-  let mongod: MongoMemoryServer;
+    let app: INestApplication;
+    let mongod: MongoMemoryServer;
+    const testUser = SeedService.testAdmin;
 
-  beforeAll(async () => {
-    ({ mongod, app } = await initializeE2ETest(mongod, app));
-  });
+    beforeAll(async () => {
+        ({ mongod, app } = await initializeE2ETest(mongod, app));
+    });
 
-  afterAll(async () => {
-    await mongod.stop();
-    await app.close();
-  });
+    afterAll(async () => {
+        await mongod.stop();
+        await app.close();
+    });
 
-  it('/auth/login (POST)', async () => {
-    const loginDto = {
-      email: process.env.ADMIN_EMAIL,
-      password: process.env.ADMIN_PASSWORD,
-    };
+    it('/auth/login (POST)', async () => {
+        const loginDto = {
+            email: testUser.email,
+            password: testUser.password,
+        };
 
-    const response = await request(app.getHttpServer())
-      .post('/auth/login')
-      .send(loginDto)
-      .expect(201);
+        const response = await request(app.getHttpServer())
+            .post('/auth/login')
+            .send(loginDto)
+            .expect(201);
 
-    expect(response.body).toHaveProperty('jwt');
-  });
+        expect(response.body).toHaveProperty('jwt');
+    });
 
-  it('/v2/auth/login (POST)', async () => {
-    const loginDto = {
-      email: process.env.ADMIN_EMAIL,
-      password: process.env.ADMIN_PASSWORD,
-    };
+    it('/v2/auth/login (POST)', async () => {
+        const loginDto = {
+            email: testUser.email,
+            password: testUser.password,
+        };
 
-    const response = await request(app.getHttpServer())
-      .post('/v2/auth/login')
-      .send(loginDto)
-      .expect(201);
+        const response = await request(app.getHttpServer())
+            .post('/v2/auth/login')
+            .send(loginDto)
+            .expect(201);
 
-    expect(response.body).toBeDefined();
-    expect(response.body).toHaveProperty('accessToken');
-    expect(response.body).toHaveProperty('refreshToken');
-  });
+        expect(response.body).toBeDefined();
+        expect(response.body).toHaveProperty('accessToken');
+        expect(response.body).toHaveProperty('refreshToken');
+    });
 
-  it('/auth/refresh (POST)', async () => {
-    const refreshToken = 'some-refresh-token';
+    it('/auth/refresh (POST)', async () => {
+        const refreshToken = 'some-refresh-token';
 
-    const loginResponse = await request(app.getHttpServer())
-        .post('/v2/auth/login')
-        .send({ email: process.env.ADMIN_EMAIL, password: process.env.ADMIN_PASSWORD })
-        .expect(201);
+        const loginResponse = await request(app.getHttpServer())
+            .post('/v2/auth/login')
+            .send({ email: testUser.email, password: testUser.password })
+            .expect(201);
 
-    const response = await request(app.getHttpServer())
-      .post('/auth/refresh')
-      .send({ refreshToken: loginResponse.body.refreshToken })
-      .expect(201);
+        const response = await request(app.getHttpServer())
+            .post('/auth/refresh')
+            .send({ refreshToken: loginResponse.body.refreshToken })
+            .expect(201);
 
-    expect(response.body).toHaveProperty('accessToken');
-  });
+        expect(response.body).toHaveProperty('accessToken');
+    });
 
-  it('/auth/refresh (POST) - should return 401 if no refresh token is provided', async () => {
-    const response = await request(app.getHttpServer())
-      .post('/auth/refresh')
-      .expect(401);
-  });
+    it('/auth/refresh (POST) - should return 401 if no refresh token is provided', async () => {
+        const response = await request(app.getHttpServer())
+            .post('/auth/refresh')
+            .expect(401);
+    });
 
-  it('/auth/register (GET)', async () => {
-    const email = 'newuser@example.com';
+    it('/auth/register (GET)', async () => {
+        const email = 'newuser@example.com';
 
-    const response = await request(app.getHttpServer())
-      .get('/auth/register')
-      .query({ email })
-      .expect(200);
+        const response = await request(app.getHttpServer())
+            .get('/auth/register')
+            .query({ email })
+            .expect(200);
 
-    expect(response.body).toHaveProperty("success", true );
-  });
+        expect(response.body).toHaveProperty("success", true);
+    });
 
-  it('/auth/register (POST) - should forbid registration for domains not registered in the system', async () => {
-    const accountSetupDto = {
-      email: 'newuser@example.com',
-      password: 'password123',
-    };
+    it('/auth/register (POST) - should forbid registration for domains not registered in the system', async () => {
+        const accountSetupDto = {
+            email: 'newuser@example.com',
+            password: 'password123',
+        };
 
-    const response = await request(app.getHttpServer())
-      .post('/auth/register')
-      .send(accountSetupDto)
-      .expect(403);
+        const response = await request(app.getHttpServer())
+            .post('/auth/register')
+            .send(accountSetupDto)
+            .expect(403);
 
-  });
+    });
 });
